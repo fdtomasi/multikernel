a59050684d39d255f7ac81549115e48e04558693
Federico Tomasi <fdtomasi@gmail.com>
2017-04-21 16:46:57 +0200

    add examples

---
+++ examples/mklSVC.py
@@ -0,0 +1,62 @@
+"""
+=============================
+Multiple Kernel Learning (MKL)
+=============================
+
+Computes an SVM where the kernel is learnt
+as a linear combination of precomputed kernels.
+
+"""
+# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
+#         Marie Szafranski <marie.szafranski@ibisc.fr>
+#
+# License: BSD Style.
+
+import numpy as np
+import pylab as pl
+
+from sklearn import svm
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
+from sklearn.utils.validation import check_is_fitted
+
+from mkl.mSVC import *
+
+if __name__ == '__main__':
+    xx, yy = np.meshgrid(np.linspace(-5, 5, 40), np.linspace(-5, 5, 40))
+    np.random.seed(0)
+    X = np.random.randn(300, 2)
+    y = np.logical_xor(X[:, 0] > 0, X[:, 1] > 0)
+    # y = X[:,0]>0
+
+    y = np.array(y, dtype=np.int)
+    y[y == 0] = -1
+
+    # Define the kernels
+    kernels = [RBF(10 ** k) for k in range(-5, 0)]  # some RBF kernels
+    kernels.append(linear)  # Add linear kernel
+
+    # fit the model
+    clf = MultiKernelSVC(kernels=kernels, C=1e6, verbose=True, maxit=100,
+                         tol=1e-5, p=1, store_objective=True)
+    clf.fit(X, y)
+
+    objective = clf.objective
+
+    pl.close('all')
+    pl.figure()
+    pl.plot(objective)
+    pl.xlabel('Iterations')
+    pl.ylabel('Dual objective')
+
+    # plot the line, the points, and the nearest vectors to the plane
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    # pl.close('all')
+    pl.figure()
+    pl.set_cmap(pl.cm.Paired)
+    pl.pcolormesh(xx, yy, Z)
+    pl.scatter(X[:,0], X[:,1], c=y)
+
+    pl.axis('tight')
+    pl.show()
---
+++ examples/supervised_regression.py
@@ -0,0 +1,52 @@
+"""An example for regression problems."""
+import numpy as np
+import matplotlib.pyplot as pl
+import seaborn as sns
+from sklearn.svm import SVR
+
+from mkl import mkl
+
+xx, yy = np.meshgrid(np.linspace(-5, 5, 40), np.linspace(-5, 5, 40))
+np.random.seed(0)
+X = np.random.randn(300, 2)
+y = np.linspace(0, 200, X.shape[0])
+# y = X[:,0]>0
+
+K1 = 1. * np.dot(X, X.T)
+K2 = 2.0 * np.dot(X, X.T)
+kernels = [K1, K2]
+
+clf = mkl.MultiKernelRegressor(
+    verbose=1, maxit=100,
+    tol=1e-5, p=1, kernel=SVR(kernel='precomputed'))
+#   store_objective=True, kernel=KernelRidge(kernel='precomputed', alpha=.5))
+clf.fit(kernels, y)
+
+print("Prediction score:", clf.score(kernels, y))
+
+# pl.close('all')
+# pl.figure()
+# pl.plot(objective)
+# pl.xlabel('Iterations')
+# pl.ylabel('Dual objective')
+
+# plot the line, the points, and the nearest vectors to the plane
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+# pl.close('all')
+pl.figure()
+pl.set_cmap(pl.cm.Paired)
+pl.pcolormesh(xx, yy, Z)
+pl.scatter(X[:,0], X[:,1], c=y)
+
+pl.axis('tight')
+pl.show()
+
+
+p =1; n_kernels = 2
+gammas = (1.0 / n_kernels) ** (1.0 / p) * np.ones(n_kernels)
+
+pl.close('all')
+sns.pointplot(X[:, 0], X[:,1], y, linestyle='')
+pl.show()
